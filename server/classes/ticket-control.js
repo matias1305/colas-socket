a711ed06aff3246f4d2f2f3170c988951531452e
Matias-Espinoza <mespinoza@ubicomp-labs.com>
2018-06-06 20:30:09 -0400

    Primer commit

---
+++ server/classes/ticket-control.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+
+class Ticket{
+   constructor(numero, escritorio){
+      this.numero = numero;
+      this.escritorio = escritorio;
+   }
+}
+
+class TicketControl {
+   
+   constructor(){
+      this.ultimo = 0;
+      this.hoy = new Date().getDate();
+      this.tickets = [];
+      this.ultimos4 = [];
+
+      let data = require('../data/data.json');
+
+      if( data.hoy === this.hoy ){
+         this.ultimo = data.ultimo;
+         this.tickets = data.tickets;
+         this.ultimos4 = data.ultimos4;
+      }else{
+         this.reiniciarConteo();
+      }
+   }
+
+   // Creamos el siguiente ticket.
+   siguiente(){
+      this.ultimo += 1;
+
+      let ticket = new Ticket(this.ultimo, null);
+      this.tickets.push(ticket);
+
+      this.grabarArchivo();
+
+      return `Ticket ${ this.ultimo }`;
+   }
+
+   // Retorna el ultimo ticket
+   getUltimoTicket(){
+      return `Ticket ${ this.ultimo }`;
+   }
+
+   // Retorna el ultimo ticket
+   getUltimos4(){
+      return this.ultimos4;
+   }
+
+   // Retorna que ticket y escritorio se atiende
+   atender(escritorio){
+      if( this.tickets.length === 0 ){
+         return "No hay tickets";
+      }
+
+      let numeroTicket = this.tickets[0].numero;
+      this.tickets.shift();
+
+      let atenderTicket = new Ticket(numeroTicket, escritorio);
+
+      this.ultimos4.unshift(atenderTicket);
+
+      if( this.ultimos4.length > 4 ){
+         this.ultimos4.splice(-1, 1); // borra el ultimo elemento
+      }
+
+      this.grabarArchivo();
+
+      return atenderTicket;
+   }
+
+   // Reiniciamos el conteo a 0;
+   reiniciarConteo(){
+      this.ultimo = 0;
+      this.tickets = [];
+      this.ultimos4 = [];
+
+      console.log("Se a inicializado el sistema");
+      this.grabarArchivo();
+   }
+
+   // Guardamos los archivos de respaldo
+   grabarArchivo(){
+      let jsonData = {
+         ultimo: this.ultimo,
+         hoy: this.hoy,
+         tickets: this.tickets,
+         ultimos4: this.ultimos4
+      }
+ 
+      let jsonDataString = JSON.stringify(jsonData);
+      fs.writeFileSync('./server/data/data.json', jsonDataString);
+   }
+
+
+}
+
+// Exportamos la clase
+module.exports = {
+   TicketControl
+};
